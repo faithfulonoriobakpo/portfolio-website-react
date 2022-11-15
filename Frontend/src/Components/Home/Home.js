80e25120d6896addb0f16890bcdf9be405d3a1b3
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-11-15 23:18:16 +0100

    Added typewrite effect and more content to home component

--- Frontend/src/Components/Home/Home.js
+++ Frontend/src/Components/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Home.css';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <div className='container-fluid home'>
       <div className='container home-content'>
-        <h1>Hi, Welcome to Faithful Onoriobakpo's Page!</h1>
+        <h1>Hi, Welcome to my Page!</h1>
+        <h3>
+          <Typewriter 
+            options={{
+              strings: ["My Name is Faithful Onoriobakpo and I'm a Frontend Developer."],
+              autoStart: true,
+              loop: true,
+              delay: 5
+            }}
+          />
+        </h3>
       </div>
     </div>
   )
