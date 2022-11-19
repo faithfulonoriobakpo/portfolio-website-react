69139438f973b829bacfe9445d65cc9f4ebd49d2
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-11-19 11:43:58 +0100

    Added and styled about photo

--- Frontend/src/Components/About/About.js
+++ Frontend/src/Components/About/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './About.css';
+import aboutPhoto from '../../Images/faithfulbwb.jpeg';
 
 const About = () => {
   return (
-    <div className='container' style={{'padding': '0 200px'}}>
-        About
+    <div className='container about-section'>
+        <div className='row'>
+            <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12'>
+                <div className='about-image'>
+                    <img src={aboutPhoto} alt='picture of faithful'/>
+                </div>
+            </div>
+            <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12'></div>
+        </div>
     </div>
   )
 }
