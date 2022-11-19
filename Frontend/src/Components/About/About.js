60c23d9a47da48c8fe9b95f4f11a8c150c6af6cc
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-11-19 09:43:10 +0100

    Addedd About component

---
+++ Frontend/src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className='container' style={{'padding': '0 200px'}}>
+        About
+    </div>
+  )
+}
+
+export default About
