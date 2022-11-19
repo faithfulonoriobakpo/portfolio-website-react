3d61bdebf712df7d137554bf5915f6414c1cab8f
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-11-19 13:03:30 +0100

    created Tech Stack component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sidebar from './Components/Sidebar/Sidebar';
 import About from './Components/About/About';
+import Techstack from './Components/Tech Stack/Techstack';
 
 const App = () => {
   return(
     <>
       <Sidebar />
       <About />
+      <Techstack />
     </>
   );
 }
---
+++ Frontend/src/Components/Tech Stack/Techstack.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Techstack.css';
+
+const Techstack = () => {
+  return (
+    <div>Techstack</div>
+  )
+}
+
+export default Techstack
