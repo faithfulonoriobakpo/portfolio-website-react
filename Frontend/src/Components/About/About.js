ff825bf38cd2d78e0684dd6ac3f811b2b96f24b0
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-11-19 18:12:17 +0100

    changed image alt to reduce screen reader redundancy

--- Frontend/src/Components/About/About.js
+++ Frontend/src/Components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className='row'>
             <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12'>
                 <div className='about-image'>
-                    <img src={aboutPhoto} alt='picture of faithful'/>
+                    <img src={aboutPhoto} alt='faithful'/>
                 </div>
             </div>
             <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12'>
