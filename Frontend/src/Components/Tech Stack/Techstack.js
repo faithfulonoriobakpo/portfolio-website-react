7c7292c0c335634e920822b3d397f83148d1142c
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-11-19 15:50:47 +0100

    Added section title for Tech Stack

--- Frontend/src/Components/Tech Stack/Techstack.js
+++ Frontend/src/Components/Tech Stack/Techstack.js
@@ -3,7 +3,14 @@ import './Techstack.css';
 
 const Techstack = () => {
   return (
-    <div>Techstack</div>
+    <div className='container techstack-section'>
+        <div className='section-title'>
+            <h5>Tech Stack</h5>
+            <span id='line'></span>
+        </div>
+        <div className='row'>
+        </div>
+    </div>
   )
 }
 
